9b06991e5a9c97f825dd9118105b36785f5e1966
Liam Edwards <liamedwards2010@gmail.com>
2020-06-02 10:48:56 +0100

    add specific get route

--- routes/blogs.js
+++ routes/blogs.js
@@ -4,6 +4,7 @@ import blogController from '../controllers/blogController';
 const blogRouter = express.Router();
 
 blogRouter.get('/', blogController.getAllBlogs);
+blogRouter.get('/:id', blogController.getBlog);
 blogRouter.post('/', blogController.postBlog);
 blogRouter.delete('/:id', blogController.deleteBlog);
 blogRouter.put('/:id', blogController.updateBlog);
